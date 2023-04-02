5ee50fcb40265112b90160defa5421e53961be43
jordidimass <jordi.dimas@gmail.com>
2023-04-02 17:50:53 -0600

    functional site milestone lol

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='bg-gradient-to-r from-gray-700 via-gray-900 to-black'>
         <Main />
         <NextScript />
       </body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,9 @@ import data from '../data.json'
 
 function LinkCard({ href, title, image }: { href: string, title: string, image ?: string }) {
   return (
-    <a href={href} className="flex items-center p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <div className="flex flex-col">
-        <h2 className="font-bold text-lg">{title}</h2>
+    <a href={href} className="flex items-center p-4 bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 w-full rounded-md hover:scale-105 transition-all border border-gray-300 mb-3 duration-300">
+      <div className="flex flex-col text-center ">
+        <h2 className="font-semibold w-full text-center">{title}</h2>
       </div>
     </a>
   );
@@ -13,7 +13,7 @@ function LinkCard({ href, title, image }: { href: string, title: string, image ?
 
 export default function Home() {
   return (
-    <div className="flex mx-auto items-center flex-col w-full justify-center mt-16">
+    <div className="flex mx-auto items-center flex-col w-full justify-center mt-16 px-8"> 
       <Image
       className='rounded-full'
         alt={data.name}
@@ -21,10 +21,11 @@ export default function Home() {
         width={120}
         height={120}
       />
-      <h1 className='font-bold mt-4 text-xl'>{data.name}</h1>
+      <h1 className='font-bold mt-4 mb-8 text-xl text-gray-100'>{data.name}</h1>
       {data.links.map((link) => (
         <LinkCard key={link.href} {...link} />
       ))}
     </div>  
   );
 }
+
