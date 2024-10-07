b38fa02a80bb17ee7ddb2a809bdb74f0c580cb2b
jordi <jordi.dimas@gmail.com>
2024-10-07 12:15:16 -0600

    changed background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
+import Particles from "@/components/ui/particles"; // adjust the path if necessary
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-	<>
-		<Component {...pageProps} />
-		<Analytics />
-	</>
-	);
+    <>
+      <div className="relative">
+        <Particles className="absolute inset-0 -z-10" />
+        <Component {...pageProps} />
+      </div>
+      <Analytics />
+    </>
+  );
 }
-
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Particles from "@/components/ui/particles";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <title> connect with me </title>
+      <title>connect with me</title>
       <body className='bg-gradient-to-r from-gray-700 via-gray-900 to-black'>
         <Main />
         <NextScript />
+        <Particles className="absolute inset-0 z-50" />
       </body>
     </Html>
-  )
+  );
 }
